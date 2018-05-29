a5b72da14b4bb80787a0ab3e81cb70931683c647
kundan-whitehat-kk <Kundan_kk52@outlook.com>
2018-05-29 19:57:09 +0530

    Fully Updated...

--- app/src/main/java/kk/techbytecare/simplechatbot/Adapter/ChatMessageAdapter.java
+++ app/src/main/java/kk/techbytecare/simplechatbot/Adapter/ChatMessageAdapter.java
@@ -14,56 +14,55 @@ import kk.techbytecare.simplechatbot.Model.ChatMessage;
 import kk.techbytecare.simplechatbot.R;
 
 public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
-    private static final int MY_MESSAGE = 0, OTHER_MESSAGE = 1, MY_IMAGE = 2, OTHER_IMAGE = 3;
+
+    private static final int MY_MESSAGE = 0, OTHER_MESSAGE = 1;
 
     public ChatMessageAdapter(Context context, List<ChatMessage> data) {
-        super(context, R.layout.item_mine_message, data);
+        super(context, R.layout.users_message, data);
     }
 
     @Override
     public int getViewTypeCount() {
-        // my message, other message, my image, other image
-        return 4;
+        return 2;
     }
 
     @Override
     public int getItemViewType(int position) {
         ChatMessage item = getItem(position);
 
-        if (item.isMine() && !item.isImage()) return MY_MESSAGE;
-        else if (!item.isMine() && !item.isImage()) return OTHER_MESSAGE;
-        else if (item.isMine() && item.isImage()) return MY_IMAGE;
-        else return OTHER_IMAGE;
+        if (item.isMine() && !item.isImage()) {
+            return MY_MESSAGE;
+        }
+        else  {
+            return OTHER_MESSAGE;
+        }
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View itemView, ViewGroup parent) {
+
         int viewType = getItemViewType(position);
+
         if (viewType == MY_MESSAGE) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_mine_message, parent, false);
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.users_message, parent, false);
 
-            TextView textView = (TextView) convertView.findViewById(R.id.text);
+            TextView textView = itemView.findViewById(R.id.text);
             textView.setText(getItem(position).getContent());
 
-        } else if (viewType == OTHER_MESSAGE) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_other_message, parent, false);
+        }
+        else if (viewType == OTHER_MESSAGE) {
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.bot_message, parent, false);
 
-            TextView textView = (TextView) convertView.findViewById(R.id.text);
+            TextView textView =  itemView.findViewById(R.id.text);
             textView.setText(getItem(position).getContent());
-        } else if (viewType == MY_IMAGE) {
-            //convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_mine_image, parent, false);
-        } else {
-            // convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_other_image, parent, false);
         }
 
-        convertView.findViewById(R.id.chatMessageView).setOnClickListener(new View.OnClickListener() {
+        itemView.findViewById(R.id.chatMessageView).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "onClick", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "Clicked", Toast.LENGTH_LONG).show();
             }
         });
-
-
-        return convertView;
+        return itemView;
     }
 }
--- app/src/main/java/kk/techbytecare/simplechatbot/MainActivity.java
+++ app/src/main/java/kk/techbytecare/simplechatbot/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.alicebot.ab.AIMLProcessor;
 import org.alicebot.ab.Bot;
@@ -32,10 +33,10 @@ import kk.techbytecare.simplechatbot.Model.ChatMessage;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private FloatingActionButton btnSend;
-    private EditText edtTextMsg;
-    private ImageView imageView;
+    ListView listView;
+    FloatingActionButton btnSend;
+    EditText edtTextMsg;
+    ImageView imageView;
 
     public Bot bot;
     public static Chat chat;
@@ -47,9 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
-        btnSend = findViewById(R.id.btn_send);
-        edtTextMsg = findViewById(R.id.et_message);
-        imageView = findViewById(R.id.iv_image);
+        btnSend = findViewById(R.id.btnSend);
+        edtTextMsg = findViewById(R.id.edtTextMsg);
+        imageView = findViewById(R.id.imageView);
+
         adapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
         listView.setAdapter(adapter);
 
@@ -57,90 +59,79 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String message = edtTextMsg.getText().toString();
-                //bot
+
                 String response = chat.multisentenceRespond(edtTextMsg.getText().toString());
                 if (TextUtils.isEmpty(message)) {
+                    Toast.makeText(MainActivity.this, "Please enter a query..", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 sendMessage(message);
-                mimicOtherMessage(response);
+                botsReply(response);
                 edtTextMsg.setText("");
                 listView.setSelection(adapter.getCount() - 1);
             }
         });
-        //checking SD card availablility
-        boolean a = isSDCARDAvailable();
-        //receiving the assets from the app directory
+
+        boolean available = isSDCARDAvailable();
+
         AssetManager assets = getResources().getAssets();
-        File jayDir = new File(Environment.getExternalStorageDirectory().toString() + "/TBC/bots/TBC");
-        boolean b = jayDir.mkdirs();
-        if (jayDir.exists()) {
+        File fileName = new File(Environment.getExternalStorageDirectory().toString() + "/TBC/bots/TBC");
+
+        boolean makeFile = fileName.mkdirs();
+
+        if (fileName.exists()) {
             //Reading the file
             try {
                 for (String dir : assets.list("TBC")) {
-                    File subdir = new File(jayDir.getPath() + "/" + dir);
-                    boolean subdir_check = subdir.mkdirs();
+
+                    File subDir = new File(fileName.getPath() + "/" + dir);
+                    boolean subDir_check = subDir.mkdirs();
+
                     for (String file : assets.list("TBC/" + dir)) {
-                        File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
-                        if (f.exists()) {
+                        File newFile = new File(fileName.getPath() + "/" + dir + "/" + file);
+                        if (newFile.exists()) {
                             continue;
                         }
-                        InputStream in = null;
-                        OutputStream out = null;
+                        InputStream in;
+                        OutputStream out;
                         in = assets.open("TBC/" + dir + "/" + file);
-                        out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
+                        out = new FileOutputStream(fileName.getPath() + "/" + dir + "/" + file);
+
                         //copy file from assets to the mobile's SD card or any secondary memory
                         copyFile(in, out);
                         in.close();
-                        in = null;
                         out.flush();
                         out.close();
-                        out = null;
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         //get the working directory
         MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/TBC";
-        System.out.println("Working Directory = " + MagicStrings.root_path);
         AIMLProcessor.extension =  new PCAIMLProcessorExtension();
-        //Assign the AIML files to bot for processing
+
         bot = new Bot("TBC", MagicStrings.root_path, "chat");
         chat = new Chat(bot);
-        String[] args = null;
-        mainFunction(args);
-
     }
 
     private void sendMessage(String message) {
         ChatMessage chatMessage = new ChatMessage(message, true, false);
         adapter.add(chatMessage);
-
-        //mimicOtherMessage(message);
     }
 
-    private void mimicOtherMessage(String message) {
+    private void botsReply(String message) {
         ChatMessage chatMessage = new ChatMessage(message, false, false);
         adapter.add(chatMessage);
     }
 
-    private void sendMessage() {
-        ChatMessage chatMessage = new ChatMessage(null, true, true);
-        adapter.add(chatMessage);
 
-        mimicOtherMessage();
-    }
-
-    private void mimicOtherMessage() {
-        ChatMessage chatMessage = new ChatMessage(null, false, true);
-        adapter.add(chatMessage);
-    }
-    //check SD card availability
     public static boolean isSDCARDAvailable(){
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)? true :false;
     }
+
     //copying the file
     private void copyFile(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[1024];
@@ -149,17 +140,4 @@ public class MainActivity extends AppCompatActivity {
             out.write(buffer, 0, read);
         }
     }
-    //Request and response of user and the bot
-    public static void mainFunction (String[] args) {
-        MagicBooleans.trace_mode = false;
-        System.out.println("trace mode = " + MagicBooleans.trace_mode);
-        Graphmaster.enableShortCuts = true;
-        Timer timer = new Timer();
-        String request = "Hello.";
-        String response = chat.multisentenceRespond(request);
-
-        System.out.println("Human: "+request);
-        System.out.println("Robot: " + response);
-    }
-
 }
